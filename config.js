fd5ebddfc05d8917a5d6a4aebf6b03a225bd5ab6
TANG, YOU-ZENG <christang426859@gmail.com>
2025-11-09 15:46:02 +0800

    support markdown in chatbox

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
-const API_BASE_URL = "https://5acf12ebf0a9.ngrok-free.app";  // change this in one place
+const API_BASE_URL = "https://e338f88c425c.ngrok-free.app";  // change this in one place
 export { API_BASE_URL };
